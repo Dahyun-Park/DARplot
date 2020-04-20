8cd0b4374bb89bb47728756c025e952fb2efbf8e
Dahyun-Park <62977441+Dahyun-Park@users.noreply.github.com>
2020-04-20 13:18:58 +0900

    Update DARplot.R

--- R/DARplot.R
+++ R/DARplot.R
@@ -13,6 +13,83 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+#######################################################tabling#############################################################
+
+inputfile <- function(dir) {
+  setwd(dir)
+  src_dir <- c(dir)
+  # listing up name of files in the directory => object
+  src_file <- list.files(src_dir) # list
+  # file list 
+  src_file_cnt <- length(src_file)
+  
+  for(i in 1:src_file_cnt){
+    rawdata_Set <- read.table(
+    paste(src_dir,"/",src_file[i],sep=""),#cut/cut_chr1.assoc.logistic
+    ,header =T,stringsAsFactors = T)
+    write.table(rawdata_Set,
+                paste(src_dir,"/","merge.tsv",sep = ""),
+                sep = "\t",
+                row.names = FALSE,
+                col.names = FALSE,
+                quote = FALSE,
+                append = TRUE) #append-> stacking
+    rm(rawdata_Set)
+    }
+  a<-read.csv("merge.tsv",sep = '\t',header=F)
+  colnames(a)<-c("CHR","SNP","BP","A1","TEST","NMISS","OR","SE","L95","U95","STAT","P")
+  a<-a[,-c(4,5,6,7,8,9,10,11)]
+  write.table(a,
+              paste(src_dir,"/","all.tsv",sep = ""),
+              sep = "\t",
+              row.names = FALSE,
+              col.names = TRUE,
+              append = TRUE,
+              quote = FALSE)
+  rm(merge)
+  
+}
+
+####################################################### annotateSNPregion ##############################################################
+annotSNP<-function(snps, chr, pos, pvalue, snplist,
+                             kbaway=0, maxpvalue=1, labels=c(), col=c(), pch=c()) {
+
+  stopifnot(all(length(snps)==length(chr), length(chr)==length(pos),
+                length(pos)==length(pvalue)))
+  if (length(snplist)==0) stop("snplist vector is empty")
+
+  if(any(pos>1e6)) kbaway<-kbaway*1000
+
+  ann<-rep(0, length(snps))
+  for(i in seq_along(snplist)) {
+    si<-which(snps==snplist[i])
+    ci<-chr[si]
+    pi<-pos[si]
+    ann[chr==ci & pos >= pi-kbaway & pos <= pi+kbaway & pvalue<=maxpvalue]<-i
+  }
+  ann<-list(factor(ann, levels=0:length(snplist), labels=c("", snplist)))
+  if(length(col)>0 || length(pch)>0 || length(labels)>0) {
+    for(i in seq_along(snplist)) {
+      ann[[ snplist[i] ]] = list()
+      if(length(col)>0) {
+        ann[[ snplist[i] ]]$col = col[ (i-1) %% length(col)+1 ]
+      }
+      if(length(pch)>0) {
+        ann[[ snplist[i] ]]$pch = pch[ (i-1) %% length(pch)+1 ]
+      }
+      if(length(labels)>0) {
+        ann[[ snplist[i] ]]$label = labels[ (i-1) %% length(labels)+1 ]
+      }
+    }
+  }
+  return(ann)
+}
+
+
+
+#######################################DAR.Plot#######################################################
+
+
 DAR.plot<-function(chr, pos, pvalue,
                    sig.level=NA, annotate=NULL, ann.default=list(),
                    should.thin=T, thin.pos.places=2, thin.logp.places=2,
@@ -213,7 +290,7 @@ DAR.plot<-function(chr, pos, pvalue,
 
 
 ####################################################### annotateSNPregion ##############################################################
-annotateSNPRegions<-function(snps, chr, pos, pvalue, snplist,
+annotSNP<-function(snps, chr, pos, pvalue, snplist,
                              kbaway=0, maxpvalue=1, labels=c(), col=c(), pch=c()) {
 
   stopifnot(all(length(snps)==length(chr), length(chr)==length(pos),
@@ -246,3 +323,5 @@ annotateSNPRegions<-function(snps, chr, pos, pvalue, snplist,
   }
   return(ann)
 }
+
+
